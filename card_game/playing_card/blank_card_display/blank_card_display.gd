6574ee820e49d9f4d0e9cc8a40e90d259bae99ba
Silvio Mayolo <mercerenies@comcast.net>
2024-03-13 00:12:08 -0400

    BlankCardDisplay variant of HiddenCardDisplay

---
+++ card_game/playing_card/blank_card_display/blank_card_display.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+func set_card(_card) -> void:
+    # No-op to make this node compatible with CardStrip.
+    pass
+
+
+func on_added_to_strip(_strip) -> void:
+    # No-op, no reaction to being added to strip.
+    pass
+
+
+func on_added_to_row(_strip) -> void:
+    # No-op, no reaction to being added to row.
+    pass
