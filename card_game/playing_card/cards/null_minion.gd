83154e7fc93715ed03ce9d235be4f05955ed3fc3
Silvio Mayolo <mercerenies@comcast.net>
2024-03-13 00:11:53 -0400

    NullMinion for front faces of back-facing cards

---
+++ card_game/playing_card/cards/null_minion.gd
@@ -0,0 +1,41 @@
+extends MinionCardType
+
+# This minion card should never appear in the game proper. It's used
+# as the front face for cards whose back is showing (e.g., this is the
+# front face of cards in the enemy's hand that you should not be able
+# to see)
+
+func get_id() -> int:
+    return 3
+
+
+func get_title() -> String:
+    return "Null Minion"
+
+
+func get_text() -> String:
+    return "Null Card"
+
+
+func get_star_cost() -> int:
+    return 8
+
+
+func get_picture_index() -> int:
+    return 0
+
+
+func get_base_level() -> int:
+    return 1
+
+
+func get_base_morale() -> int:
+    return 1
+
+
+func get_archetypes() -> Array:
+    return []
+
+
+func get_rarity() -> int:
+    return Rarity.COMMON
