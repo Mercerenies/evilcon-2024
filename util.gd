0186c0bedfecb39e7faf40265c3fd5d14ee2ceb9
Silvio Mayolo <mercerenies@comcast.net>
2024-03-16 20:14:56 -0400

    Note on util function

--- util.gd
+++ util.gd
@@ -8,6 +8,10 @@ func free_all_children(node: Node):
 
 
 func queue_free_all_children(node: Node):
+    # Note: We could just do child.queue_free() for each child, but it
+    # makes sense to await the idle frame only once and then free all
+    # of them once it's safe to do so, rather than queuing up several
+    # deletions separately on the same idle frame.
     var children = node.get_children()
     await get_tree().process_frame
     for child in children:
