e2bc3bb44eec87d8cedeb8343350176d0753644c
Silvio Mayolo <mercerenies@comcast.net>
2024-03-28 11:09:08 -0400

    remove debug code

--- card_game/playing_card/playing_card_display/playing_card_display.gd
+++ card_game/playing_card/playing_card_display/playing_card_display.gd
@@ -30,7 +30,6 @@ func _process(_delta: float) -> void:
 func _update_display() -> void:
     $Card/TitleLabel.text = card_type.get_title()
     $Card/TextLabel.text = BBCodeModifier.process_bbcode(card_type.get_text())
-    print($Card/TextLabel.text)
     $Card/ArchetypesRow.icons = card_type.get_icon_row()
     $Card/CostRow.icons = Util.filled_array(CardIcon.Frame.EVIL_STAR, card_type.get_star_cost())
     $Card/CardPicture.frame = card_type.get_picture_index()
