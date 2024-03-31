e47a91ef289c6e5bd6d53e7ea56f746fd09baabd
Silvio Mayolo <mercerenies@comcast.net>
2024-03-31 15:27:29 -0400

    note for spare battery ui

--- card_game/playing_card/cards/spare_battery.gd
+++ card_game/playing_card/cards/spare_battery.gd
@@ -44,6 +44,7 @@ func on_play(playing_field, card) -> void:
         })
     else:
         var most_powerful_robot = Util.max_by(minions, CardEffects.card_power_less_than(playing_field))
+        # TODO Make these two displays not overlap in the UI
         await Stats.add_level(playing_field, most_powerful_robot, 1)
         await Stats.add_morale(playing_field, most_powerful_robot, 1)
     await CardGameApi.destroy_card(playing_field, card)
