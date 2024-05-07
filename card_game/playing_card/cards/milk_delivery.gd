94930a375a830fd3486d878e3af723b05ed2bd3d
Silvio Mayolo <mercerenies@comcast.net>
2024-05-07 15:31:37 -0400

    better UI for milk delivery

--- card_game/playing_card/cards/milk_delivery.gd
+++ card_game/playing_card/cards/milk_delivery.gd
@@ -30,6 +30,14 @@ func on_play(playing_field, card) -> void:
     var owner = card.owner
     var minion_strip = playing_field.get_minion_strip(owner)
     await CardGameApi.highlight_card(playing_field, card)
-    for minion in minion_strip.cards().card_array():
-        await Stats.add_morale(playing_field, minion, 1)
+
+    if len(minions) == 0:
+        var card_node = CardGameApi.find_card_node(playing_field, card)
+        Stats.play_animation_for_stat_change(playing_field, card_node, 0, {
+            "custom_label_text": Stats.NO_TARGET_TEXT,
+            "custom_label_color": Stats.NO_TARGET_COLOR,
+        })
+    else:
+        for minion in minion_strip.cards().card_array():
+            await Stats.add_morale(playing_field, minion, 1)
     await CardGameApi.destroy_card(playing_field, card)
