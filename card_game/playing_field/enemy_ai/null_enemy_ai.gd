cf390bb8e396ca09f592c78f8b39c35a02e72356
Silvio Mayolo <mercerenies@comcast.net>
2024-10-27 16:29:27 -0400

    Remove old EnemyAI system

--- card_game/playing_field/enemy_ai/null_enemy_ai.gd
+++
@@ -1,7 +0,0 @@
-extends EnemyAI
-
-# Basic Enemy AI that never does anything. Used for debugging.
-
-func on_enemy_turn_start(playing_field) -> void:
-    # Immediately end enemy turn
-    end_enemy_turn(playing_field)
