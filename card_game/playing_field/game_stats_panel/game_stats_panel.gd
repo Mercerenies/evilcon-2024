6433ddbdb5054335db70fd8f565b57f47c11caec
Silvio Mayolo <mercerenies@comcast.net>
2024-10-29 14:36:26 -0400

    Bugfix for non-visual game stats

--- card_game/playing_field/game_stats_panel/game_stats_panel.gd
+++ card_game/playing_field/game_stats_panel/game_stats_panel.gd
@@ -4,6 +4,8 @@ const DESTINY_SONG_LIMIT := 3
 
 const DEFAULT_FORT_DEFENSE := 60
 
+@export var is_showing_visuals := true
+
 # Note: Stats which are themselves stateful (e.g., fort defense, max
 # fort defense, current evil point count, and current destiny song
 # count) are stored here as instance variables. That is, this object
@@ -57,22 +59,26 @@ func get_destiny_song_node() -> Node2D:
 
 
 func _update_evil_points() -> void:
-    $EvilPointsStat.text = "%s/%s" % [evil_points, _evil_points_per_turn]
+    if is_showing_visuals:
+        $EvilPointsStat.text = "%s/%s" % [evil_points, _evil_points_per_turn]
 
 
 func _update_hand_limit(playing_field, player: StringName) -> void:
-    var hand_size = playing_field.get_hand(player).cards().card_count()
-    var hand_limit = StatsCalculator.get_hand_limit(playing_field, player)
-    $HandLimitStat.text = "%s/%s" % [hand_size, hand_limit]
+    if is_showing_visuals:
+        var hand_size = playing_field.get_hand(player).cards().card_count()
+        var hand_limit = StatsCalculator.get_hand_limit(playing_field, player)
+        $HandLimitStat.text = "%s/%s" % [hand_size, hand_limit]
 
 
 func _update_fort_defense() -> void:
-    $FortDefenseStat.text = "%s/%s" % [fort_defense, max_fort_defense]
+    if is_showing_visuals:
+        $FortDefenseStat.text = "%s/%s" % [fort_defense, max_fort_defense]
 
 
 func _update_destiny_song() -> void:
-    $DestinySongStat.text = "%s/%s" % [destiny_song, DESTINY_SONG_LIMIT]
-    $DestinySongStat.visible = (destiny_song > 0)
+    if is_showing_visuals:
+        $DestinySongStat.text = "%s/%s" % [destiny_song, DESTINY_SONG_LIMIT]
+        $DestinySongStat.visible = (destiny_song > 0)
 
 
 func update_stats_from(playing_field, player: StringName) -> void:
@@ -81,3 +87,11 @@ func update_stats_from(playing_field, player: StringName) -> void:
     _update_hand_limit(playing_field, player)
     _update_fort_defense()
     _update_destiny_song()
+
+
+func load_stats_from(other_stats_panel) -> void:
+    evil_points = other_stats_panel.evil_points
+    _evil_points_per_turn = other_stats_panel._evil_points_per_turn
+    fort_defense = other_stats_panel.fort_defense
+    max_fort_defense = other_stats_panel.max_fort_defense
+    destiny_song = other_stats_panel.destiny_song
