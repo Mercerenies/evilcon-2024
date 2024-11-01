6765ed0872c0873ff4dcb7a61e355a5f2e8d1481
Silvio Mayolo <mercerenies@comcast.net>
2024-11-01 14:54:23 -0400

    Clean up Disembodied Soul logic (query API)

--- card_game/playing_card/cards/disembodied_soul.gd
+++ card_game/playing_card/cards/disembodied_soul.gd
@@ -9,7 +9,7 @@ func get_title() -> String:
 
 
 func get_text() -> String:
-    return "Disembodied Soul has +1 Level for each [icon]UNDEAD[/icon] UNDEAD in your discard pile, up to a maximum of 5."
+    return "Disembodied Soul has +1 Level for each [icon]UNDEAD[/icon] UNDEAD Minion in your discard pile, up to a maximum of 5."
 
 
 func get_picture_index() -> int:
@@ -37,16 +37,6 @@ func get_rarity() -> int:
 
 
 func get_level(playing_field, card) -> int:
-    var own_discards = playing_field.get_discard_pile(card.owner).cards().card_array()
-    var discarded_undeads = own_discards.filter(_is_undead_card_type)
     var starting_level = super.get_level(playing_field, card)
-    return starting_level + mini(len(discarded_undeads), 5)
-
-
-func _is_undead_card_type(card_type):
-    if not (card_type is MinionCardType):
-        return false
-    # NOTE: get_base_archetypes since we're not in play and thus don't
-    # have archetype modifiers.
-    var archetypes = card_type.get_base_archetypes()
-    return Archetype.UNDEAD in archetypes
+    var undeads_count = Query.on(playing_field).discard_pile(card.owner).count(Query.by_archetype(Archetype.UNDEAD))
+    return starting_level + mini(undeads_count, 5)
