ad622d2e1aa39325f7daec8922c9fae5845c7e91
Silvio Mayolo <mercerenies@comcast.net>
2024-11-02 11:44:17 -0400

    Mastermime valuation (AI)

--- card_game/playing_card/cards/the_mastermime.gd
+++ card_game/playing_card/cards/the_mastermime.gd
@@ -50,3 +50,12 @@ func on_play_broadcasted(playing_field, this_card, played_card) -> void:
         Stats.show_text(playing_field, played_card, PopupText.CLOWNED)
         played_card.metadata[CardMeta.ARCHETYPE_OVERRIDES] = [Archetype.CLOWN]
     playing_field.emit_cards_moved()
+
+
+func ai_get_score(playing_field, player: StringName, priorities) -> float:
+    var score = super.ai_get_score(playing_field, player, priorities)
+    # As of Nov 2, 2024, the average EP cost of a playing card is 3.2,
+    # so assume that the opponent is playing 2 or 3 cards per turn.
+    # Here, we assume the opponent is playing 2 Minion cards per turn.
+    score += get_base_morale() * 2 * priorities.of(LookaheadPriorities.CLOWNING)
+    return score
