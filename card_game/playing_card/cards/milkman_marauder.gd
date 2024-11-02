d1dc5d47ba6a4e6a1549a76c4603f7e4d4c3aec0
Silvio Mayolo <mercerenies@comcast.net>
2024-11-02 12:07:30 -0400

    Milkman Marauder AI valuation

--- card_game/playing_card/cards/milkman_marauder.gd
+++ card_game/playing_card/cards/milkman_marauder.gd
@@ -52,3 +52,27 @@ func on_expire(playing_field, this_card) -> void:
     else:
         for minion in minions:
             await Stats.add_morale(playing_field, minion, 1)
+
+
+func ai_get_score(playing_field, player: StringName, priorities) -> float:
+    var score = super.ai_get_score(playing_field, player, priorities)
+    # As of Nov 2, 2024, the average Level of a Minion is 1.7. Assume
+    # you will control 2 Minions when Milkman Marauder expires, for a
+    # total of a 3.4 damage boost to the enemy's fort.
+    score += 3.4 * priorities.of(LookaheadPriorities.FORT_DEFENSE)
+    return score
+
+
+func ai_get_score_broadcasted(playing_field, this_card, player: StringName, priorities, target_card_type) -> float:
+    var score = super.ai_get_score_broadcasted(playing_field, this_card, player, priorities, target_card_type)
+    if this_card.owner != player:
+        return score
+    if not (target_card_type is MinionCardType):
+        return score
+
+    # If we control Milkman Marauder, then prioritize playing Minions
+    # that will outlast him and therefore benefit from his effect.
+    if target_card_type.get_base_morale() >= get_morale(playing_field, this_card):
+        score += target_card_type.get_base_level() * priorities.of(LookaheadPriorities.FORT_DEFENSE)
+
+    return score
--- debug/debug_room.gd
+++ debug/debug_room.gd
@@ -56,7 +56,7 @@ func _debug_interactive_game():
 
     $PlayingField.turn_number = 10  # Get extra EP :)
     #$PlayingField.get_hand(CardPlayer.BOTTOM).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.GIGGLES_GALORE))
-    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MAXWELL_STERLING))
+    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MILKMAN_MARAUDER))
     #$PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.UNPAID_INTERN))
     #$PlayingField.get_minion_strip(CardPlayer.TOP).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.BABY_CLOWN), CardPlayer.TOP))
     #$PlayingField.get_minion_strip(CardPlayer.TOP).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.UNPAID_INTERN), CardPlayer.TOP))
