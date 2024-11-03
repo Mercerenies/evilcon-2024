074d778c7576644f28940171af4bc2118c0c3f9f
Silvio Mayolo <mercerenies@comcast.net>
2024-11-03 12:29:20 -0500

    De-buff milk delivery
    It now applies to all minions, meaning playing it at a bad time can
    benefit your opponent

--- card_game/playing_card/cards/milk_delivery.gd
+++ card_game/playing_card/cards/milk_delivery.gd
@@ -10,11 +10,11 @@ func get_title() -> String:
 
 
 func get_text() -> String:
-    return "+1 Morale to all of your Minions."
+    return "+1 Morale to all Minions currently in play, regardless of owner."
 
 
 func get_star_cost() -> int:
-    return 4
+    return 2
 
 
 func get_picture_index() -> int:
@@ -27,22 +27,26 @@ func get_rarity() -> int:
 
 func on_play(playing_field, card) -> void:
     await super.on_play(playing_field, card)
-    var owner = card.owner
-    var minion_strip = playing_field.get_minion_strip(owner)
     await CardGameApi.highlight_card(playing_field, card)
 
-    if minion_strip.cards().card_count() == 0:
+    var minions = Query.on(playing_field).minions().array()
+
+    if len(minions) == 0:
         Stats.show_text(playing_field, card, PopupText.NO_TARGET)
     else:
-        for minion in minion_strip.cards().card_array():
+        for minion in minions:
             await Stats.add_morale(playing_field, minion, 1)
     await CardGameApi.destroy_card(playing_field, card)
 
 
 func ai_get_score(playing_field, player: StringName, priorities) -> float:
     var score = super.ai_get_score(playing_field, player, priorities)
-    score += (
+    var defense_points = (
         Query.on(playing_field).minions(player)
         .map_sum(Query.level().value())
+    ) - (
+        Query.on(playing_field).minions(CardPlayer.other(player))
+        .map_sum(Query.level().value())
     )
+    score += defense_points * priorities.of(LookaheadPriorities.FORT_DEFENSE)
     return score
