e3471ac8ecbb6eee7bd4eaae11f91132c792c81e
Silvio Mayolo <mercerenies@comcast.net>
2024-11-07 19:12:50 -0500

    cover of moonlight AI valuation

--- card_game/playing_card/cards/cover_of_moonlight.gd
+++ card_game/playing_card/cards/cover_of_moonlight.gd
@@ -35,3 +35,52 @@ func do_broadcasted_influence_check(playing_field, card, target_card, source_car
             Stats.show_text(playing_field, target_card, PopupText.BLOCKED)
         return false
     return super.do_broadcasted_influence_check(playing_field, card, target_card, source_card, silently)
+
+
+func ai_get_score(playing_field, player: StringName, priorities) -> float:
+    var score = super.ai_get_score(playing_field, player, priorities)
+
+    # See how many cards are in play and NOT protected by Ninja
+    # immunity.
+    var hypothetical_attacker = PlayingCardCodex.get_entity(PlayingCardCodex.ID.PLUMBERMAN)
+    var unprotected_minions = (
+        Query.on(playing_field).minions(player)
+        .filter(Query.influenced_by(hypothetical_attacker, CardPlayer.other(player)))
+        .map_sum(Query.remaining_ai_value().value())
+    )
+    score += unprotected_minions * priorities.of(LookaheadPriorities.IMMUNITY)
+
+    return score
+
+
+func ai_get_score_per_turn(playing_field, player: StringName, priorities) -> float:
+    var score = super.ai_get_score_per_turn(playing_field, player, priorities)
+
+    # Any unprotected Minions get an extra turn of immunity. This
+    # slightly overcounts, if the Minion in question expires, but we
+    # don't know exactly how many extra turns we're considering
+    # gaining.
+    var hypothetical_attacker = PlayingCardCodex.get_entity(PlayingCardCodex.ID.PLUMBERMAN)
+    var unprotected_minions = (
+        Query.on(playing_field).minions(player)
+        .filter(Query.influenced_by(hypothetical_attacker, CardPlayer.other(player)))
+        .map_sum(Query.remaining_ai_value().value())
+    )
+    score += unprotected_minions * priorities.of(LookaheadPriorities.IMMUNITY)
+
+    return score
+
+
+func ai_get_score_broadcasted(playing_field, this_card, player: StringName, priorities, target_card_type) -> float:
+    var score = super.ai_get_score_broadcasted(playing_field, this_card, player, priorities, target_card_type)
+    if this_card.owner != player:
+        return score
+
+    # Newly-played Minions are immune, so increase the score unless
+    # that Minion already had immunity from another source.
+    if target_card_type is MinionCardType:
+        var hypothetical_attacker = PlayingCardCodex.get_entity(PlayingCardCodex.ID.PLUMBERMAN)
+        var hypothetical_defender = Card.new(target_card_type, player)
+        if CardEffects.do_hypothetical_influence_check(playing_field, hypothetical_defender, hypothetical_attacker, CardPlayer.other(player)):
+            score += priorities.of(LookaheadPriorities.IMMUNITY) * target_card_type.ai_get_expected_remaining_score(playing_field, null)
+    return score
