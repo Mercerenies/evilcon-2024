a593dd5a454baf59df54e5b8e52a2a2fa37ba26d
Silvio Mayolo <mercerenies@comcast.net>
2024-11-09 11:44:00 -0500

    Ninja mask valuation (AI)

--- card_game/playing_card/cards/ninja_mask.gd
+++ card_game/playing_card/cards/ninja_mask.gd
@@ -41,3 +41,16 @@ func _evaluate_effect(playing_field, this_card) -> void:
         return
     most_powerful_minion.metadata[CardMeta.HAS_SPECIAL_IMMUNITY] = true
     playing_field.emit_cards_moved()
+
+
+func ai_get_score(playing_field, player: StringName, priorities) -> float:
+    var score = super.ai_get_score(playing_field, player, priorities)
+
+    var target = CardEffects.most_powerful_minion(playing_field, player)
+    if target != null:
+        var hypothetical_attacker = PlayingCardCodex.get_entity(PlayingCardCodex.ID.PLUMBERMAN)
+        var is_unprotected = CardEffects.do_hypothetical_influence_check(playing_field, target, hypothetical_attacker, CardPlayer.other(player))
+        if is_unprotected:
+            score += priorities.of(LookaheadPriorities.IMMUNITY) * target.card_type.ai_get_expected_remaining_score(playing_field, target)
+
+    return score
