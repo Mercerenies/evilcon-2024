deb06abd39cace8e4fc1f7af3684fb6de805c2e0
Silvio Mayolo <mercerenies@comcast.net>
2025-07-09 21:10:17 -0400

    AI valuation greener pastures

--- card_game/playing_card/cards/greener_pastures.gd
+++ card_game/playing_card/cards/greener_pastures.gd
@@ -33,3 +33,10 @@ func on_play(playing_field, card) -> void:
     await CardGameApi.highlight_card(playing_field, card)
     await CardEffects.power_up_archetype(playing_field, card, TARGET_ARCHETYPES)
     await CardGameApi.destroy_card(playing_field, card)
+
+
+func ai_get_score(playing_field, player: StringName, priorities) -> float:
+    return (
+        super.ai_get_score(playing_field, player, priorities) +
+        CardEffects.ai_score_for_powering_up_archetype(playing_field, self, player, TARGET_ARCHETYPES, priorities)
+    )
--- debug/debug_room.gd
+++ debug/debug_room.gd
@@ -62,7 +62,7 @@ func _debug_interactive_game():
     top_deck.cards().shuffle()
 
     $PlayingField.turn_number = 10  # Get extra EP :)
-    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.FARMERS_MARKET))
+    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.GREENER_PASTURES))
     #$PlayingField.get_hand(CardPlayer.BOTTOM).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.KING_TURTLE))
     #$PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.DR_BADGUY_DOOMCAKE))
     #$PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.KING_TURTLE), CardPlayer.BOTTOM))
