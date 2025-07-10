c535b6462ad85cc9c5b020fe488895d3d19d3e2e
Silvio Mayolo <mercerenies@comcast.net>
2025-07-09 20:19:09 -0400

    Defensive formation ai valuation

--- card_game/playing_card/cards/defensive_formation.gd
+++ card_game/playing_card/cards/defensive_formation.gd
@@ -62,6 +62,85 @@ func augment_attack_damage(playing_field, this_card, attacking_card) -> int:
     return super.augment_attack_damage(playing_field, this_card, attacking_card)
 
 
+func ai_get_score_per_turn(playing_field, player: StringName, priorities) -> float:
+    var score = super.ai_get_score_per_turn(playing_field, player, priorities)
+    if not _owner_has_any_turtles(playing_field, player):
+        # No turtles, no effect
+        return score
+    score += _ai_get_score_for_extra_turn(playing_field, player, priorities)
+    return score
+
+
+func _ai_get_score_for_extra_turn(playing_field, player: StringName, priorities) -> float:
+    var score = 0.0
+    # Skips an attack from each Minion every turn.
+    for minion in Query.on(playing_field).minions(CardPlayer.other(player)).array():
+        var can_influence = CardEffects.do_hypothetical_influence_check(playing_field, minion, self, player)
+        if can_influence:
+            score += minion.card_type.get_level(playing_field, minion) * priorities.of(LookaheadPriorities.FORT_DEFENSE)
+
+    # Blocks attacks from your own TURTLEs every turn.
+    for minion in Query.on(playing_field).minions(player).array():
+        if minion.has_archetype(playing_field, Archetype.TURTLE):
+            score -= minion.card_type.get_level(playing_field, minion) * priorities.of(LookaheadPriorities.FORT_DEFENSE)
+
+    return score
+
+
 func _owner_has_any_turtles(playing_field, owner) -> bool:
     var minions = playing_field.get_minion_strip(owner).cards().card_array()
     return minions.any(func(c): return c.has_archetype(playing_field, Archetype.TURTLE))
+
+
+func ai_get_score_broadcasted(playing_field, this_card, player: StringName, priorities, target_card_type) -> float:
+    var score = super.ai_get_score_broadcasted(playing_field, this_card, player, priorities, target_card_type)
+
+    if this_card.owner == player:
+        score += _ai_get_score_broadcasted_friendly(playing_field, this_card, priorities, target_card_type)
+    else:
+        score += _ai_get_score_broadcasted_enemy(playing_field, this_card, priorities, target_card_type)
+    return score
+
+
+func _ai_get_score_broadcasted_friendly(playing_field, this_card, priorities, target_card_type) -> float:
+    # If the target card type is a TURTLE and we don't currently have
+    # a TURTLE, then it's worth it to play it in order to keep this
+    # card on the field.
+    var score = 0.0
+    if not (target_card_type is MinionCardType):
+        return score
+    if not (Archetype.TURTLE in target_card_type.get_base_archetypes()):
+        return score
+
+    if not _owner_has_any_turtles(playing_field, this_card.owner):
+        var turns_left = get_total_turn_count() - this_card.metadata[CardMeta.TURN_COUNTER]
+        score += _ai_get_score_for_extra_turn(playing_field, this_card.owner, priorities) * turns_left
+
+    # Will block the next attack from this TURTLE
+    score -= target_card_type.get_base_level() * priorities.of(LookaheadPriorities.FORT_DEFENSE)
+
+    return score
+
+
+func _ai_get_score_broadcasted_enemy(playing_field, this_card, priorities, target_card_type) -> float:
+    var score = 0.0
+    if not _owner_has_any_turtles_with_good_morale(playing_field, this_card.owner):
+        return score
+    if not (target_card_type is MinionCardType):
+        return score
+
+    # Will block one attack from this Minion
+    var turns_left = get_total_turn_count() - this_card.metadata[CardMeta.TURN_COUNTER]
+    if turns_left > 0:
+        score -= target_card_type.get_base_level() * priorities.of(LookaheadPriorities.FORT_DEFENSE)
+
+    return score
+
+
+func _owner_has_any_turtles_with_good_morale(playing_field, owner) -> bool:
+    return (
+        Query.on(playing_field)
+        .minions(owner)
+        .filter(Query.morale().greater_than(1))
+        .any(Query.by_archetype(Archetype.TURTLE))
+    )
--- debug/debug_room.gd
+++ debug/debug_room.gd
@@ -61,10 +61,14 @@ func _debug_interactive_game():
     top_deck.cards().shuffle()
 
     $PlayingField.turn_number = 10  # Get extra EP :)
-    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.TURTLES_UNITE))
+    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.TINY_TURTLE))
+    $PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.DEFENSIVE_FORMATION))
     #$PlayingField.get_hand(CardPlayer.BOTTOM).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.KING_TURTLE))
     #$PlayingField.get_hand(CardPlayer.TOP).cards().push_card(PlayingCardCodex.get_entity(PlayingCardCodex.ID.DR_BADGUY_DOOMCAKE))
-    #$PlayingField.get_minion_strip(CardPlayer.TOP).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.CORDYCEPS), CardPlayer.TOP))
+    $PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MILITARY_ANDROID), CardPlayer.BOTTOM))
+    $PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MILITARY_ANDROID), CardPlayer.BOTTOM))
+    $PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MILITARY_ANDROID), CardPlayer.BOTTOM))
+    $PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MILITARY_ANDROID), CardPlayer.BOTTOM))
     #$PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.KING_TURTLE), CardPlayer.BOTTOM))
     #$PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.MILKMAN_MARAUDER), CardPlayer.BOTTOM))
     #$PlayingField.get_minion_strip(CardPlayer.BOTTOM).cards().push_card(Card.new(PlayingCardCodex.get_entity(PlayingCardCodex.ID.TURKEY), CardPlayer.BOTTOM))
@@ -94,7 +98,7 @@ func _sample_deck():
         PlayingCardCodex.get_entity(PlayingCardCodex.ID.RED_TURTLE),
         PlayingCardCodex.get_entity(PlayingCardCodex.ID.BUSY_BEE),
         PlayingCardCodex.get_entity(PlayingCardCodex.ID.WORKER_BEE),
-        PlayingCardCodex.get_entity(PlayingCardCodex.ID.WORKER_BEE),
+        PlayingCardCodex.get_entity(PlayingCardCodex.ID.DEFENSIVE_FORMATION),
         PlayingCardCodex.get_entity(PlayingCardCodex.ID.GOLDEN_ACORN),
         PlayingCardCodex.get_entity(PlayingCardCodex.ID.SPIKY_RED_TURTLE),
         PlayingCardCodex.get_entity(PlayingCardCodex.ID.KING_TURTLE),
