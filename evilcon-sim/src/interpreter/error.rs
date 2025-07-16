30441cb8221b6c615f4a9b638c7e30ac3c40584b
Mercerenies <mercerenies@gmail.com>
2025-07-16 13:33:15 -0400

    more error types

--- evilcon-sim/src/interpreter/error.rs
+++ evilcon-sim/src/interpreter/error.rs
@@ -1,5 +1,5 @@
 
-use super::value::InvalidHashKey;
+use super::value::{InvalidHashKey, NoSuchVar, NoSuchFunc};
 
 use thiserror::Error;
 
@@ -11,4 +11,20 @@ pub enum EvalError {
   UnknownClass(String),
   #[error("Poisoned constant")]
   PoisonedConstant,
+  #[error("Undefined variable {0}")]
+  UndefinedVariable(String),
+  #[error("Unknown function {0}")]
+  UndefinedFunc(String),
+}
+
+impl From<NoSuchVar> for EvalError {
+  fn from(e: NoSuchVar) -> Self {
+    EvalError::UndefinedVariable(e.0)
+  }
+}
+
+impl From<NoSuchFunc> for EvalError {
+  fn from(e: NoSuchFunc) -> Self {
+    EvalError::UndefinedFunc(e.0)
+  }
 }
