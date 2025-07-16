129eaf41f6b226d444408ee3a140474b9c9ed40d
Mercerenies <mercerenies@gmail.com>
2025-07-16 13:33:08 -0400

    identifier helpers

--- evilcon-sim/src/ast/identifier.rs
+++ evilcon-sim/src/ast/identifier.rs
@@ -1,7 +1,15 @@
 
+use std::borrow::Borrow;
+
 /// Thin wrapper around a string, marking it as an identifier.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Identifier(pub String);
+
+impl Identifier {
+  pub fn new(s: impl Into<String>) -> Self {
+    Self(s.into())
+  }
+}
 
 impl From<String> for Identifier {
   fn from(s: String) -> Self {
@@ -18,5 +26,17 @@ impl<'a> From<&'a str> for Identifier {
 impl From<Identifier> for String {
   fn from(i: Identifier) -> Self {
     i.0
+  }
+}
+
+impl AsRef<str> for Identifier {
+  fn as_ref(&self) -> &str {
+    &self.0
+  }
+}
+
+impl Borrow<str> for Identifier {
+  fn borrow(&self) -> &str {
+    &self.0
   }
 }
