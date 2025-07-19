6e882673766306ef3105a3114f5ef69f78abc76e
Mercerenies <mercerenies@gmail.com>
2025-07-19 16:20:43 -0400

    reviewed CardPlayer dependencies (there weren't many :))

--- evilcon-sim/src/driver.rs
+++ evilcon-sim/src/driver.rs
@@ -12,6 +12,7 @@ pub const GDSCRIPT_FILES: &[&str] = &[
   "../card_game/playing_field/player_agent/player_agent.gd",
   "../card_game/playing_field/player_agent/lookahead_ai_agent/lookahead_ai_agent.gd",
   "../card_game/playing_field/player_agent/lookahead_ai_agent/lookahead_priorities.gd",
+  "../card_game/playing_field/card_player.gd",
   "../util.gd",
   "../card_game/playing_card/playing_card_lists.gd",
 ];
