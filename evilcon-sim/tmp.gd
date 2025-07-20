2f19c4ad29d7ec8c431716316658775b21a17ce9
Mercerenies <mercerenies@gmail.com>
2025-07-19 20:56:57 -0400

    finished event_logger code review

--- evilcon-sim/tmp.gd
+++ evilcon-sim/tmp.gd
@@ -2,9 +2,13 @@
 # NOTE: This file is not used for anything other than testing. Delete me someday plz :)
 
 static func test():
+    var z = RefCounted.new()
     var a = [[0]]
     var b = a.duplicate()
     var c = a.duplicate(true)
     a[0].push_back(1)
     a[0].append(2)
-    return [a, b, c]
+
+    z.aaa = 0
+
+    return z
