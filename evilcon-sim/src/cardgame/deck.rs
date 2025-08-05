d87a14c45edd834483d3d2fd07a0f4c75d386a5f
Silvio Mayolo <mercerenies@comcast.net>
2025-08-04 21:06:02 -0400

    impl FromStr for Deck

--- evilcon-sim/src/cardgame/deck.rs
+++ evilcon-sim/src/cardgame/deck.rs
@@ -1,4 +1,9 @@
 
+use thiserror::Error;
+
+use std::str::FromStr;
+use std::num::ParseIntError;
+
 pub const DECK_SIZE: usize = 20;
 
 /// Newtype wrapper around a vector of [`CardId`].
@@ -8,6 +13,13 @@ pub struct Deck(pub Vec<CardId>);
 /// The ID of a playing card.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CardId(pub i64);
+
+#[derive(Debug, Clone, Error)]
+#[non_exhaustive]
+pub enum DeckFromStrError {
+  #[error("{0}")]
+  ParseIntError(#[from] ParseIntError),
+}
 
 impl Deck {
   pub fn is_empty(&self) -> bool {
@@ -28,5 +40,42 @@ impl AsRef<[CardId]> for Deck {
 impl FromIterator<CardId> for Deck {
   fn from_iter<I: IntoIterator<Item = CardId>>(iter: I) -> Self {
     Self(iter.into_iter().collect())
+  }
+}
+
+impl FromStr for Deck {
+  type Err = DeckFromStrError;
+
+  /// Parses comma-separated list of integers. Ignores whitespace
+  /// around numbers.
+  fn from_str(s: &str) -> Result<Self, DeckFromStrError> {
+    let cards = s.split(',')
+      .map(|s| s.trim())
+      .filter(|s| !s.is_empty())
+      .map(|s| s.parse::<i64>().map(CardId))
+      .collect::<Result<Vec<_>, _>>()?;
+    Ok(Deck(cards))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_deck_from_str() {
+    let deck: Deck = "1, 2, 3, 4, 5".parse().unwrap();
+    assert_eq!(deck.0, vec![CardId(1), CardId(2), CardId(3), CardId(4), CardId(5)]);
+  }
+
+  #[test]
+  fn test_deck_from_str_empty() {
+    let deck: Deck = "".parse().unwrap();
+    assert!(deck.is_empty(), "Deck {:?} is not empty", deck);
+  }
+
+  #[test]
+  fn test_deck_from_str_invalid() {
+    "a".parse::<Deck>().unwrap_err();
   }
 }
